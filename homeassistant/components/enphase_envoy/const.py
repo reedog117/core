59a4fe7b48620fed4def49857a15f5d24f5874b4
Patrick C <reedog117@users.noreply.github.com>
2022-02-07 00:27:22 -0500

    Add device_class to current power measurements

--- homeassistant/components/enphase_envoy/const.py
+++ homeassistant/components/enphase_envoy/const.py
@@ -22,6 +22,7 @@ SENSORS = (
         name="Current Power Production",
         native_unit_of_measurement=POWER_WATT,
         state_class=SensorStateClass.MEASUREMENT,
+        device_class=SensorDeviceClass.ENERGY,
     ),
     SensorEntityDescription(
         key="daily_production",
@@ -49,6 +50,7 @@ SENSORS = (
         name="Current Power Consumption",
         native_unit_of_measurement=POWER_WATT,
         state_class=SensorStateClass.MEASUREMENT,
+        device_class=SensorDeviceClass.ENERGY,
     ),
     SensorEntityDescription(
         key="daily_consumption",
